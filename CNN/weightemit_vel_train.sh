85adc3b461d4ea34655921ab5ddb90cfc986ac50
connorfjennings <connorfjennings.gmail.com>
2025-11-11 13:46:09 -0500

    tar

---
+++ CNN/weightemit_vel_train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=weightgas_vel_train
+#SBATCH --output=weightemit_vel_out.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --time=3:00:00   
+#SBATCH --mem=30G
+#SBATCH --gpus=1
+#SBATCH --constraint="a5000|v100|a100"
+
+module purge
+module load miniconda
+module load CUDA
+conda activate torch
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+srun python seg_models_train.py packed_aug8_weightemitvel weightemit_vel
+
